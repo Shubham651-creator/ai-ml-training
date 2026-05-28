64a2cd9b209c3f5cef38aa94231dee5088b07ccd
Shubham651-creator <shubhamawale651@gmail.com>
2026-05-28 10:10:44 +0000

    Day11: Ensemble Learning- random forest & boosting

---
+++ 02_Machine_Learning/Day11/day11.md
@@ -0,0 +1,22 @@
+# Ensemble Learning
+> Combine multiple models to create strong predicatin system.
+
+- Instead of one genius model, ML often prefer many average model together
+
+## Random Forest Algrithm
+> Many decision tree together and training each differently.
+
+- Parallel independent trees and learnings
+
+1. Each tree gets random subset of data & random subset of feature
+2. Train many trees
+3. combine predications- majority voting
+
+## Boosting
+> Each model tries to fix previous model error (sequentially learning)
+
+### 1. Adaboost (Adapative Boosting)
+- Reweight samples
+
+### 2. Gradient Boosting
+- learns residual error
---
+++ 02_Machine_Learning/Day11/random_forest_algo.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score
+)
+
+# dataset
+data = {
+    "Hours_Studied": [1,2,3,4,5,6,7,8],
+    "Pass": [0,0,0,0,1,1,1,1]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+X = df[["Hours_Studied"]]
+y = df["Pass"]
+
+# split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# train
+model = RandomForestClassifier(
+    n_estimators = 10,
+    random_state=42
+)
+model.fit(X_train, y_train)
+
+# predict
+y_pred = model.predict(X_test)
+# y_pred = model.predict_proba(X_test)
+
+print("Predictions:", y_pred)
+
+# accuracy
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:",
+      precision_score(y_test, y_pred))
+
+print("Recall:",
+      recall_score(y_test, y_pred))
+
+print("F1:",
+      f1_score(y_test, y_pred))
